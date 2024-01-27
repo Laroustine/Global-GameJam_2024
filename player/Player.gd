129cc5a981717b6fee0e3fd5c3397b28a7dff8b6
Paul Charrier <paul.charrier1978@outlook.com>
2024-01-27 22:21:43 +0100

    added timer that returns to main menu when over and it's display

--- player/Player.gd
+++ player/Player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed:int = 400
 @export var map_size:Vector2 = Vector2(2048,2048)
+@onready var time_lable = $Control/Time
+@export var timer:Timer 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +11,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time_lable.set_text(time_to_str(int(timer.time_left)))
 
+func time_to_str(time):
+	var min = int(time)/60
+	var sec = int(time) - min*60
+	return str("%0*d"%[2,min])+":"+str("%0*d" % [2,sec])
+	
 func _physics_process(delta):
 	# returns the direction with analog support
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
