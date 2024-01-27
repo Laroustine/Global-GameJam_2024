761badf54db6de49b1300ac86af1f8f6f790125c
Paul Charrier <paul.charrier1978@outlook.com>
2024-01-27 14:34:10 +0100

    added analog support to movement and camera follows player

--- player/Player.gd
+++ player/Player.gd
@@ -1,30 +1,26 @@
 extends Area2D
 
-@export var speed = 400
-var screen_size
+@export var speed:int = 400
+@export var map_size:Vector2 = Vector2(2048,2048)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	pass
+
+func _physics_process(delta):
+	# returns the direction with analog support
+	var velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+
 	
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	#position = position.clamp(Vector2.ZERO, map_size)
