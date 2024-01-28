469e9d2359b2e21f39be80f0da5ed147eb130145
Justin Blard <justin.blard.dev@gmail.com>
2024-01-28 03:18:10 +0100

    [ADD] Handle of Mini-Game Withing player

--- player/Player.gd
+++ player/Player.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 @export var speed:int = 400
 @export var map_size:Vector2 = Vector2(2048,2048)
 @onready var time_lable = $Control/Time
+@onready var score_lable = $Control/Score
 @export var timer:Timer 
+var SCORE = 0
+
+var IS_PLAYING = false
+var MINI_GAMES = [preload("res://mini-games/meme_ortie.tscn")]
+var CURRENT_GAME = null
 
 signal collided_child(child)
 
@@ -12,15 +18,18 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	time_lable.set_text(time_to_str(int(timer.time_left)))
+	score_lable.set_text("Score: " + str(SCORE))
 
-func time_to_str(time):
-	var min = int(time)/60
-	var sec = int(time) - min*60
+func time_to_str(time: int):
+	var min = time/60
+	var sec = time - min*60
 	return str("%0*d"%[2,min])+":"+str("%0*d" % [2,sec])
 	
-func _physics_process(delta):
+func _physics_process(_delta):
+	if IS_PLAYING:
+		return
 	# returns the direction with analog support
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
@@ -29,11 +38,23 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-
-	
 	move_and_slide()
 
+func set_playing(value: bool):
+	IS_PLAYING = value
+	$Control/SubViewportContainer.visible = value
+	if CURRENT_GAME:
+		CURRENT_GAME.IS_PLAYING = value
 
 func _on_area_2d_area_entered(area):
+	CURRENT_GAME = MINI_GAMES[randi_range(0, len(MINI_GAMES) - 1)].instantiate()
+	CURRENT_GAME.give_score.connect(_on_give_score)
+	$Control/SubViewportContainer/SubViewport.add_child(CURRENT_GAME)
+	set_playing(true)
 	collided_child.emit(area)
-	
+
+func _on_give_score(value):
+	SCORE += value
+	set_playing(false)
+	CURRENT_GAME.queue_free()
+	CURRENT_GAME = null
